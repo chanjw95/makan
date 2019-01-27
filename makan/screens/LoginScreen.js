362438cacdb5ac0770c6d125bb43f8c200a5e9b8
Jesse Li <abyssalcloud@gmail.com>
2019-01-26 23:21:07 -0700

    Updating Login Page Appearance
    Updating Login Page Appearance
    -Adding temp logo picture (two options)
    -Color Combiation: Red/White

--- makan/screens/LoginScreen.js
+++ makan/screens/LoginScreen.js
@@ -17,27 +17,57 @@ import { MonoText } from '../components/StyledText';
 
 export default class LoginScreen extends React.Component {
   static navigationOptions = {
-    title: 'Login'
+   header: null
+   /* title: '',
+    headerStyle: {
+      backgroundColor: '#E74C3C'
+    },
+    */
   };
   render() {
     const {navigate} = this.props.navigation;
     return (
       <ScrollView style={styles.container}>
+      <View style={styles.logoContainer}>
+      <Image  
+        style={styles.logo}
+        source ={ require('../assets/images/logo-white.png')} 
+        />
+     </View> 
+     
+     <View style={styles.buttons} >
         <Button
-          title="Customer"
+          title="Customer Login"
           onPress={() => navigate("CustLogin", {screen: "CustomerLoginScreen"})}
-          color="red"
+          color="white"
+          
         />
+        </View>
+        <View style={styles.buttons} >
         <Button
-          title="Cook"
+          title="Cook Login"
           onPress={() => navigate("CookLogin", {screen: "CookLoginScreen"})}
-          color="green"
+          color="white" 
         />
+        </View>
+        <View style={styles.buttons} >
         <Button
-          title="Kitchen"
+
+          title="Kitchen Login"
           onPress={() => navigate("KitchenLogin", {screen: "KitchenLoginScreen"})}
-          color="blue"
+          color="white" 
         />
+        </View>
+      
+       <View style = {styles.signupTextCont}>
+         <Text style = {styles.signupText}> Don't have an account yet?  </Text>
+          <Button 
+          title="SignUp"
+          onPress={() => navigate("SignUp", {screen: "SignUpScreen"})}
+          color="white"  
+        />
+
+       </View> 
       </ScrollView>
     );
   };
@@ -47,6 +77,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF420E',
+    justifyContent:'center'
   },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent:'center',
+    paddingVertical:75 
+  },
+  logo:{  
+    width: 150,
+    height: 150 
+
+  },
+  buttons: { 
+       
+    backgroundColor: '#F98866' ,
+    flexGrow: 1, 
+    justifyContent:'center',
+    alignItems: 'center',
+    marginVertical:10,
+    marginHorizontal:50,
+    paddingVertical:10 ,
+    borderRadius: 30
+  } ,
+  signupTextCont: {
+  flexGrow: 1, 
+    alignItems: 'center',
+    justifyContent:'center',
+    paddingVertical:35 
+     
+  },
+  signupText: {
+  color: 'rgba(255,255,255,0.8)',
+  fontSize: 16
+  } 
 });
