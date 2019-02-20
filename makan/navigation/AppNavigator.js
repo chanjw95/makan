7abb2f2bd70d35bda5af34925b710f2066a9d250
Jesse Li <abyssalcloud@gmail.com>
2019-02-20 07:09:25 -0700

    Blank Main Page
    -Blank main page created
    -routed app navigator to main page

--- makan/navigation/AppNavigator.js
+++ makan/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 import LoginScreen from '../screens/LoginScreen';
+import MainPageScreen from '../screens/MainPageScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import CustomerLoginScreen from '../screens/CustomerLoginScreen';
 import CookLoginScreen from '../screens/CookLoginScreen';
@@ -9,6 +10,7 @@ import KitchenLoginScreen from '../screens/KitchenLoginScreen';
 import MainTabNavigator from './MainTabNavigator';
 
 const AuthStack = createStackNavigator({ Login: LoginScreen });
+const MainPage = createStackNavigator({ MainPage: MainPageScreen });
 const SignUp = createStackNavigator({ SignUp: SignUpScreen });
 const Login = createStackNavigator({ Login: LoginScreen });
 const CustomerLoginStack = createStackNavigator({ CustomerLogin: CustomerLoginScreen });
@@ -20,6 +22,7 @@ export default createAppContainer(createSwitchNavigator({
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
   Auth: AuthStack,
+  MainPage: MainPageScreen,
   Login: LoginScreen,
   SignUp: SignUpScreen,
   CustLogin: CustomerLoginStack,
@@ -27,6 +30,6 @@ export default createAppContainer(createSwitchNavigator({
   KitchenLogin: KitchenLoginStack,
 },
 {
-  initialRouteName: "Auth",
+  initialRouteName: "MainPage",
 }
 ));
---
+++ makan/screens/MainPageScreen.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  KeyboardAvoidingView
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { WebBrowser } from 'expo';
+import { ExpoLinksView } from '@expo/samples';
+import { MonoText } from '../components/StyledText'; 
+
+export default class MainPageScreen extends React.Component {
+  static navigationOptions = {
+   header: null
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+
+       <KeyboardAvoidingView behavior="padding" style={styles.container}>
+
+        <View style = {styles.signupTextCont}>
+
+         <Text style = {styles.signupText}> Don't have an account yet?  </Text>
+           <TouchableOpacity
+          style={styles.buttons}
+          onPress={() => navigate("SignUp", {screen: "SignUpScreen"})}>
+
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+ 
+
+      </KeyboardAvoidingView>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#FF420E',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: 20
+  },   
+  buttonText:{
+    textAlign:'center',
+    color: '#FFFFFF',
+    fontWeight:'700'
+
+  },
+
+  signupTextCont: {
+    alignItems: 'center',
+    paddingVertical:40,
+
+  },
+  signupText: {
+  color: 'rgba(255,255,255,0.8)',
+  fontSize: 16
+  }
+});
